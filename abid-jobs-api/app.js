b27bcd2aa2f63a2b83b1969081e4860a206f241d
ssjabid <abid.rakha1@hotmail.com>
2023-10-01 15:00:41 +0100

    adding swagger yaml documentation to app.js

--- abid-jobs-api/app.js
+++ abid-jobs-api/app.js
@@ -9,6 +9,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 // connectDB
 const connectDB = require("./db/connect");
 const authenticateUser = require("./middleware/authentication");
@@ -32,9 +37,11 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.get("/", (req, res) => {
-  res.send("jobs api");
+  res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+
 // extra packages
 
 // routes
